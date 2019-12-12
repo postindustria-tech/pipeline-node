fb2821e99dfb2e7a1b1070eda66c9dd19dbfd7c4
Steve Ballantine <Stephen@51degrees.com>
2019-12-12 14:33:19 +0000

    REFACTOR: Move device detection and location engines to separate repositories. Also add license file and update source notices.
    - Move device detection and location engines to separate repositories.
    - Add license file
    - Add license and copyright notice to source files.

--- fiftyone.pipeline.devicedetection/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    deviceDetectionCloud: require("./deviceDetectionCloud"),
-    deviceDetectionOnPremise: require("./deviceDetectionOnPremise"),
-    deviceDetectionPipelineBuilder: require("./deviceDetectionPipelineBuilder"),
-    swigData: require("./swigData"),
-    swigHelpers: require("./swigHelpers")
-
-}
