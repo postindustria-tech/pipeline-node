d90aeddb54625c5d137310019af851d7db8a2420
Steve Ballantine <Stephen@51degrees.com>
2019-12-23 12:38:03 +0000

    BUILD: Added Node 6 and 12 to testing matrix. Made some adjustments to the automated build and release process.
    - Build and test pipeline now runs for Node 6 and 12 as well as 8 and 10.
    - Publish pipeline now creates tgz files rather than publishing directly to the internal feed. These can then be published to other locations like the internal feed or NPM as needed using an automated release pipeline.
    - Added polyfill to handle missing Object.values and Object.entries methods in Node 6.

--- fiftyone.pipeline.cloudrequestengine/cloudRequestEngine.js
+++ fiftyone.pipeline.cloudrequestengine/cloudRequestEngine.js
@@ -23,6 +23,27 @@ let require51 = (requestedPackage) => {
     }
 }
 
+(function () {
+
+    const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+    const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+    const concat = Function.bind.call(Function.call, Array.prototype.concat);
+    const keys = Reflect.ownKeys;
+
+    if (!Object.values) {
+        Object.values = function values(O) {
+            return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+        };
+    }
+
+    if (!Object.entries) {
+        Object.entries = function entries(O) {
+            return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+        };
+    }
+
+}());
+
 const engine = require51("fiftyone.pipeline.engines").engine;
 const querystring = require("querystring");
 const cloudHelpers = require("./cloudHelpers");
--- fiftyone.pipeline.javascriptbundler/javascriptBundler.js
+++ fiftyone.pipeline.javascriptbundler/javascriptBundler.js
@@ -25,6 +25,27 @@ let require51 = (requestedPackage) => {
 
 const core = require51("fiftyone.pipeline.core");
 
+(function () {
+
+    const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+    const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+    const concat = Function.bind.call(Function.call, Array.prototype.concat);
+    const keys = Reflect.ownKeys;
+
+    if (!Object.values) {
+        Object.values = function values(O) {
+            return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+        };
+    }
+
+    if (!Object.entries) {
+        Object.entries = function entries(O) {
+            return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+        };
+    }
+
+}());
+
 const flowElement = core.flowElement;
 const elementDataDictionary = core.elementDataDictionary;
 
