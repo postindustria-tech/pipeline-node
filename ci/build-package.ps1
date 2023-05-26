09ea5656b2cfd636b4bbd0c04d37c9e30e47e165
romansertsov <roman.sertsov@postindustria.com>
2023-05-26 21:22:55 +0300

    npm secret added

--- ci/build-package.ps1
+++ ci/build-package.ps1
@@ -3,6 +3,8 @@ param (
     [string]$RepoName
 )
 
-./node/build-package-npm.ps1 -RepoName $RepoName
+$packages = "fiftyone.pipeline.cloudrequestengine", "fiftyone.pipeline.core", "fiftyone.pipeline.engines", "fiftyone.pipeline.engines.fiftyone"
+
+./node/build-package-npm.ps1 -RepoName $RepoName -Packages $packages
 
 exit $LASTEXITCODE
