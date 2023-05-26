d1f74a2876ff6a7d84c789c7ff5346e3d3ac5c3e
romansertsov <roman.sertsov@postindustria.com>
2023-05-26 19:51:37 +0300

    build package ps1

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -3,6 +3,8 @@ param (
     [string]$RepoName
 )
 
-./node/publish-package-npm.ps1 -RepoName $RepoName
+$packages = "fiftyone.pipeline.cloudrequestengine", "fiftyone.pipeline.core", "fiftyone.pipeline.engines", "fiftyone.pipeline.engines.fiftyone"
+
+./node/publish-package-npm.ps1 -RepoName $RepoName -Packages $packages
 
 exit $LASTEXITCODE
