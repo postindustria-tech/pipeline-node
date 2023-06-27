9ae5ee157c65766dfba16047003f46b036a33996
ben51degrees <ben@51degrees.com>
2023-06-27 15:16:58 +0100

    BUG: Keys object was used when not available.

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -1,9 +1,10 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$RepoName
+    [string]$RepoName,
+    [string]$TestResourceKey
 )
 
-$env:RESOURCE_KEY = $Keys.TestResourceKey
+$env:RESOURCE_KEY = $TestResourceKey
 
 ./node/run-integration-tests.ps1 -RepoName $RepoName
 
