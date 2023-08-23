4a4ae1b0e62c5f2bae8bb7d4f7cc1f847ba31df1
romansertsov <roman.sertsov@postindustria.com>
2023-08-23 12:31:14 +0300

    upstream merged remote packages dependencies changed to n.n.x

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -1,10 +1,11 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [boolean]$DryRun
-)
-
-./node/publish-package-npm.ps1 -RepoName $RepoName -DryRun $DryRun
-
-exit $LASTEXITCODE
+Write-Output "PUBLISH SKIPPED"
+#param (
+#    [Parameter(Mandatory=$true)]
+#    [string]$RepoName,
+#    [Parameter(Mandatory=$true)]
+#    [boolean]$DryRun
+#)
+#
+#./node/publish-package-npm.ps1 -RepoName $RepoName -DryRun $DryRun
+#
+#exit $LASTEXITCODE
