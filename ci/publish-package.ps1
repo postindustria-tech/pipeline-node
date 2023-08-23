4486ec0520c4b6e0dde1e7c02de51a6a6badb6f0
romansertsov <roman.sertsov@postindustria.com>
2023-08-23 15:23:37 +0300

    publish package enabled

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -1,11 +1,10 @@
-Write-Output "PUBLISH SKIPPED"
-#param (
-#    [Parameter(Mandatory=$true)]
-#    [string]$RepoName,
-#    [Parameter(Mandatory=$true)]
-#    [boolean]$DryRun
-#)
-#
-#./node/publish-package-npm.ps1 -RepoName $RepoName -DryRun $DryRun
-#
-#exit $LASTEXITCODE
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [boolean]$DryRun
+)
+
+./node/publish-package-npm.ps1 -RepoName $RepoName -DryRun $DryRun
+
+exit $LASTEXITCODE
