97f441f721fcf88ad812548cee692e05ca9a0b1f
romansertsov <roman.sertsov@postindustria.com>
2023-08-25 18:47:56 +0300

    packages added to update script

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -3,7 +3,7 @@ param (
     [string]$RepoName
 )
 
-$packages = ""
+$packages = "fiftyone.pipeline.cloudrequestengine", "fiftyone.pipeline.core", "fiftyone.pipeline.engines", "fiftyone.pipeline.engines.fiftyone"
 
 ./node/update-packages.ps1 -RepoName $RepoName -Packages $packages
 
